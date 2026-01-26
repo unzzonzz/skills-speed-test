d5b04265561afcfa4b6a2e54b5808034400685b4
seungchan0911 <imrealsexy00@gmail.com>
2026-01-26 10:25:47 +0900

    Complete

--- 선수제공파일/B8/script.js
+++ 선수제공파일/B8/script.js
@@ -13,6 +13,12 @@ function showMessage(isSuccess) {
     const div = document.createElement('div')
     div.classList.add('message')
     div.textContent = isSuccess ? '성공하였습니다' : '실패하였습니다'
+    div.classList.add(isSuccess ? 'success' : 'fail')
     
     $('.messages').append(div)
+
+    div.addEventListener('click', (e) => {
+        e.target.remove()
+    })
+    setTimeout(() => div.remove(), 5000)
 }
